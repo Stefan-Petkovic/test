478b5d7004d19e241d16fed3729e1400162fb4a8
unknown <Stefan>
2023-02-06 13:46:48 +0100

    Initial Commit

---
+++ JavaHelloWorldpetlja.java
@@ -0,0 +1,9 @@
+public class JavaHelloWorld
+{
+       public static void main(String[] args
+       {
+           for(int=o; i<5' i++) {
+               System.outprintln(x:"HelloWorld");
+           }
+        }
+}
